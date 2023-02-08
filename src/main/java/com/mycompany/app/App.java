b190eb283e39d9a2dd44dc767ed3158c2838877f
Ramya Rathore <ramyasri19400@gmail.com>
2023-02-08 12:39:28 +0530

    newcommit

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,13 +1,13 @@
 package com.mycompany.app;
 
 /**
- * Hello world!!
+ * Hello world!
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println("Hello World!");
     }
 }
